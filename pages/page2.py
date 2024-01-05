d1f2a2b8a2213529f42e3a46cda051f189abcc70
Shiyi Shen <shiys3447@yahoo.com>
2024-01-05 10:53:47 -0500

    update

--- pages/page2.py
+++ pages/page2.py
@@ -61,23 +61,101 @@ authors["name"] = authors["name"].str.replace("\n", "")
 
 
 
-layout = html.Div([html.H5("The Text-based Ideal Points Model"),
+layout = html.Div([html.H5("Text-based Ideal Point Generated for Author's Political Leaning"),
 html.Hr(),
-html.P([html.A("TBIP", href="https://www.aclweb.org/anthology/2020.acl-main.475/"), """ is an unsupervised probabilistic topic model called (Keyon V., Suresh N., David B. et al.) evaluates texts to quantify the political stances of their authors. The model does not require any text labeled with an ideology, nor does it use political parties or votes. Instead, it assesses the latent political viewpoints of text writers and how per-topic word choice varies according to the author's political stance ("ideological topics") given a corpus of political text and the authors of each document. The default corpus for this Colab notebook is """,html.A("Senate speeches", href="https://data.stanford.edu/congress_text"), """ from the 114th Senate session (2015-2017). The project also used the following corpora: Tweets from 2022 Democratic presidential candidates.""",]),
+html.P([dcc.Markdown("""To continue with our project objective of measuring and investigating how a politician's internal motivation aligns with their external actions (disbursements and expenditure with respect to their political acitivities and agenda; which we have disclosed the most basic information in the previous section, or [C4FE Exploratory Data Analysis](https://c4fe-tbip.onrender.com/page1), this section conducted a statistical topic modeling over the authors/politicians' tweets."""),html.A("TBIP", href="https://www.aclweb.org/anthology/2020.acl-main.475/"), """ is an unsupervised probabilistic topic model called (Keyon V., Suresh N., David B. et al.) evaluates texts to quantify the political stances of their authors. The model does not require any text labeled with an ideology, nor does it use political parties or votes.""", html.P(""""""),dcc.Markdown("""Instead, it assesses the `latent political viewpoints` of text writers and how `per-topic word choice` varies according to the author's political stance `("ideological topics")` given a corpus of political text and the authors of each document."""),
+"""Below are trained results for the list of 2022 federal election candidates' ideal points and topic aggregation of their Twitter archive.""",
+dbc.Row(
+    [dbc.Col(dbc.Row(dbc.Col(children=[html.Label(['Select State'], style={'font-size': '13px', "text-align": "left", "color": "#808080"}), dcc.Dropdown(pd.DataFrame(pd.read_csv("./data/states.csv"))['name'].tolist(),id='state-dropdown-p2')],id='states-col-p2'))),
+    dbc.Col(dbc.Row(dbc.Col(children=[html.Label(['Select Candidate'], style={'font-size': '13px', "text-align": "left", "off-set":4, "color": "#808080"}), dcc.Dropdown(id='names-dropdown-p2')],id='cand-names-col-p2'),id='cand-names-row-p2')),
+
+    ]),
+
+dbc.Row([
+dbc.Col([
+    dcc.Graph(id='bar-graph-plotly', figure={})
+]),
+dbc.Col(children=[
+    dag.AgGrid(
+        id='grid',
+        rowData=authors.to_dict("records"),
+        columnDefs=[{"field": i} for i in authors.columns],
+        columnSize="sizeToFit",
+        style={"text-align": "center"}
+    )
+], md=20, style={"margin": "2rem 0 0 0"}),
+], className='mt-4',style={"text-align": "center"}),
+html.P(""),
+html.Br(),
+html.H5("Ideological Topic Distrbution with Variational Encoding"),
+html.Hr(),
+"""The model performs inference using """,html.A("variational inference.", href="https://arxiv.org/abs/1601.00670"),""" with """, html.A("reparameterization", href="https://arxiv.org/abs/1312.6114"),html.A(" gradients.", href="https://arxiv.org/abs/1401.4082"), 
+"""What this means in plain language is that imagine you have a bunch of data, like pictures of cats. Each cat picture can be described by a set of features—things like the color of the fur, the size of the ears, and the length of the tail. Now, let's say you want to understand the hidden or latent factors that contribute to these features. The challenge is that there might be some randomness or uncertainty in these latent factors.""","""
+                
+            1. Encoding the Data:""",html.Code("""
+    The variational encoder takes each cat picture and encodes it into a set of numbers that represent the key features. However, instead of giving you a single set 
+    of numbers. It provides a distribution. This distribution tells you how likely different values of these features are."""),"""
+                                                                                                                                              
+            2. Introducing Randomness:""",html.Code("""
+    To account for uncertainty in the latent factors, the encoder introduces a bit of randomness into the encoding process. 
+    This means that even for the same cat picture, you might get slightly different sets of numbers each time due to this randomness."""),"""        
+                                                                                                                                                            
+            3. Measuring Latent Distribution:""",html.Code("""
+    The distribution you get from the variational encoder reflects the uncertainty or variability in the latent factors. 
+    It's like saying, "Well, we're not exactly sure about the size of the ears, but here's a range of possibilities, and here's how likely each possibility is."
+    """),"""
+            4. Sampling from the Distribution:""",html.Code("""
+    Now, when you want to generate or understand new cat pictures, you can sample from this distribution. Sampling means picking a specific set of numbers from 
+    the distribution. Since the distribution captures the uncertainty, each sample gives you a slightly different version of the cat picture, exploring 
+    the potential variations."""),"""
+                                         
+In the case of this project, the algorithm for topic modeling used is an extension of another popular algorithm the Latent Dirichlet Allocation (LDA).
+
+Variational inference is a method used to approximate complex probability distributions, and it plays a crucial role in Bayesian modeling. In the context of LDA and topic modeling, variational inference helps approximate the posterior distribution of latent variables, such as the distribution of topics in documents and words in topics.
+
+In variational inference, you need to specify a family of distributions from which you will choose an approximation to the true (but often intractable) posterior distribution. This family of distributions is called the "variational family."
+
+In topic modeling, the variational family represents the distributions of topics in documents and words in topics. It defines the form of the approximating distributions that the variational inference algorithm will consider. Common choices for the variational family include mean-field variational families.""","""
+
+            Mean-Field Variational Family:""",html.Code("""
+    In mean-field variational inference, it is assumed that the posterior distribution factorizes across latent variables. 
+    This means that each latent variable is assumed to be independent of the others given certain parameters. 
+    For LDA, these parameters might include the distribution of topics in documents and the distribution of words in topics.
+"""),"""
+            1. Initialize Parameters:""",html.Code("""
+    Start with some initial parameters for the variational family.
+"""),"""
+            2. Optimization Process:""",html.Code("""
+    Optimize these parameters to make the approximating distribution as close as possible to the true posterior distribution.
+"""),"""
+            3. Inference:""",html.Code("""
+    Once optimized, the parameters of the variational family give you an approximation of the posterior distribution, including the distributions of topics 
+    in documents and words in topics.
+"""),"""
+            4. Projecting Latent Information:""",html.Code("""
+    The parameters of the variational family can be interpreted as the estimated distributions of topics. 
+    These distributions are used to project latent information onto the documents and words. 
+    Each document gets a distribution over topics, and each topic gets a distribution over words."""),"""
+
+            5. Discovering Latent Topics:""",html.Code("""
+    The algorithm, through this variational inference process, discovers latent topics in the corpora based on how words co-occur across documents.
+"""),"""
+In summary, variational inference, with the help of a variational family, allows us to approximate complex posterior distributions in topic modeling. It helps uncover latent topics and their distributions in a collection of documents, providing valuable insights into the underlying thematic structures.""", """The default corpus for this Colab notebook is """ ,html.A("Senate speeches", href="https://data.stanford.edu/congress_text"), """ from the 114th Senate session (2015-2017). The project also used the following corpora: Tweets from 2022 Democratic presidential candidates.""",]),
+
+
+
 html.P(["""To replicate the whole process with my own Twitter data, I followed the steps below:""",
 dcc.Markdown("""
 
         * `counts.npz`: a `[num_documents, num_words]` [sparse CSR matrix](https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.sparse.csr_matrix.html) containing the word counts for each document.
         * `author_indices.npy`: a `[num_documents]` vector where each entry is an integer in the set `{0, 1, ..., num_authors - 1}`, indicating the author of the corresponding document in `counts.npz`.
-        * `vocabulary.txt`: a `[num_words]`-length file where each line denotes the corresponding word in the vocabulary.
-        * `author_map.txt`: a `[num_authors]`-length file where each line denotes the name of an author in the corpus.
+        * `vocabulary.txt`: a `[num_words]` - length file where each line denotes the corresponding word in the vocabulary.
+        * `author_map.txt`: a `[num_authors]` - length file where each line denotes the name of an author in the corpus.
 
 """),
-"""Perform Inference: the model performs inference using """,html.A("variational inference", href="https://arxiv.org/abs/1601.00670"),
-""" with """, html.A("reparameterization", href="https://arxiv.org/abs/1312.6114"),html.A(" gradients.", href="https://arxiv.org/abs/1401.4082"), html.P(""),
-dcc.Markdown("""Because it is intractable to evaluate the posterior distribution $p(\\theta, \\beta, \\eta, x | y)$, so the posterior is estimated with a distribution $q_\\phi(\\theta, \\beta,\\eta,x)$, parameterized by $\\phi$ through minimizing the KL-Divergence between $q$ and the posterior, which is equivalent to maximizing the ELBO:
-                            
-The variational family is set to be the mean-field family, meaning the latent variables factorize over documents $d$, topics $k$, and authors $$s$$:""",mathjax=True),
+dcc.Markdown("""Again, because it is intractable to evaluate the posterior distribution $p(\\theta, \\beta, \\eta, x | y)$, so the posterior is estimated with a distribution $q_\\phi(\\theta, \\beta,\\eta,x)$, parameterized by $\\phi$ through minimizing the KL-Divergence between $q$ and the posterior (put simple is the distance between these two distributions), which is equivalent to maximizing the ELBO (or the Evidence Lower Bound):""", mathjax=True),dcc.Markdown("""
+        $$\\mathbb{L}_{\\theta,\phi}(\\mathbf{x})=\mathbb{E}_{q_{\\phi}(\\mathbf{z}|\\mathbf{x})}[\\log p_{\\theta}(\\mathbf{x},\\mathbf{z})-\\log q_{\\phi}(\\mathbf{z}|\\mathbf{x})]$$""",mathjax=True,style={"text-align": "center"}), dcc.Markdown(                    
+"""The variational family is set to be the mean-field family, meaning the latent variables factorize over documents $d$, topics $k$, and authors $$s$$:""", mathjax=True),
 dbc.Row([dcc.Markdown(
 """$$q_\\phi(\\theta, \\beta, \\eta, x) = \\prod_{d,k,s} q(\\theta_d)q(\\beta_k)q(\\eta_k)q(x_s).$$""",mathjax=True)], style={"text-align": "center"}),
 dcc.Markdown("""
@@ -105,36 +183,7 @@ dbc.Row([dcc.Markdown("""
 dcc.Markdown("""The corresponding variational distribution is `ideological_topic_distribution`.
             
 Please checkout this [notebook](https://colab.research.google.com/github/pyro-ppl/numpyro/blob/5291d0627d68598cf78b8ea97c540268660925c1/notebooks/source/tbip.ipynb) for the full implementation in Python.
-            """)]),
-html.P(""),
-html.Br(),
-html.H5("Ideological Topics and Ideal Points Generated for Author's Political Leaning"),
-html.Hr(),
-dcc.Markdown("""Below are trained results for the list of 2022 federal election candidates' ideal points and topic aggregation of their Twitter archive
-    """),
-dbc.Row(
-    [dbc.Col(dbc.Row(dbc.Col(children=[html.Label(['Select State'], style={'font-size': '13px', "text-align": "left", "color": "#808080"}), dcc.Dropdown(pd.DataFrame(pd.read_csv("./data/states.csv"))['name'].tolist(),id='state-dropdown-p2')],id='states-col-p2'))),
-    dbc.Col(dbc.Row(dbc.Col(children=[html.Label(['Select Candidate'], style={'font-size': '13px', "text-align": "left", "off-set":4, "color": "#808080"}), dcc.Dropdown(id='names-dropdown-p2')],id='cand-names-col-p2'),id='cand-names-row-p2')),
-
-    ]),
-
-dbc.Row([
-dbc.Col([
-    dcc.Graph(id='bar-graph-plotly', figure={})
-]),
-dbc.Col(children=[
-    dag.AgGrid(
-        id='grid',
-        rowData=authors.to_dict("records"),
-        columnDefs=[{"field": i} for i in authors.columns],
-        columnSize="sizeToFit",
-        style={"text-align": "center"}
-    )
-], md=20, style={"margin": "2rem 0 0 0"}),
-], className='mt-4',style={"text-align": "center"}),
-html.Br(),
-html.Br(),
-html.Br()
+            """)])
 ],className='page2',style=PAGE_STYLE)
 
 @callback(
